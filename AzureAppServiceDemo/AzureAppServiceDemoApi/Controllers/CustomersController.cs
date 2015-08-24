9a85157e3a1037ddaff14df14b7eb3c5ebe4c5a9
Richard L. Taylor Jr. <rtaylor@rightincode.com>
2015-08-24 00:48:39 -0400

    Added Demo Api App

---
+++ AzureAppServiceDemo/AzureAppServiceDemoApi/Controllers/CustomersController.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web.Http;
+using System.Web.Http.Cors;
+using AzureAppServiceDemoApi.Models;
+using Dapper;
+
+namespace AzureAppServiceDemoApi.Controllers
+{
+    [RoutePrefix("api/customers")]
+    [EnableCors(origins: "*", headers: "*", methods: "*")]
+    public class CustomersController : ApiController
+    {
+        // GET api/customers
+        [HttpGet]
+        public IEnumerable<Customer> Get()
+        {
+            var customerData = new CustomerStore();
+            return customerData.GetCustomers();
+        }
+
+        // POST api/customers
+        [HttpPost]
+        public Customer Post(Customer newCustomer)
+        {
+            var customerData = new CustomerStore();
+            return customerData.SaveCustomer(newCustomer);
+        }
+    }
+
+    public class CustomerStore
+    {
+        public IEnumerable<Customer> GetCustomers()
+        {
+            IEnumerable<Customer> loadedCustomers;
+
+           //var dbConnectionString = ConfigurationManager.ConnectionStrings["LocalDbConnection"].ConnectionString;
+           var dbConnectionString = ConfigurationManager.ConnectionStrings["AzureDbConnection"].ConnectionString;
+
+            using (var dbConnection = new SqlConnection(dbConnectionString))
+            {
+                dbConnection.Open();
+
+                loadedCustomers = dbConnection.Query<Customer>("[dbo].[GetCustomers]", commandType: CommandType.StoredProcedure);
+            }
+
+            return loadedCustomers;
+        }
+
+        public Customer SaveCustomer(Customer newCustomer)
+        {
+            //var dbConnectionString = ConfigurationManager.ConnectionStrings["LocalDbConnection"].ConnectionString;
+            var dbConnectionString = ConfigurationManager.ConnectionStrings["AzureDbConnection"].ConnectionString;
+
+            using (var dbConnection = new SqlConnection(dbConnectionString))
+            {
+                dbConnection.Open();
+
+                var parms = new DynamicParameters();
+                parms.Add("@FirstName", newCustomer.FirstName, DbType.String, ParameterDirection.Input);
+                parms.Add("@LastName", newCustomer.LastName, DbType.String, ParameterDirection.Input);
+                parms.Add("@Email", newCustomer.Email, DbType.String, ParameterDirection.Input);
+                parms.Add("@Phone", newCustomer.Phone, DbType.String, ParameterDirection.Input);
+
+                var customers = dbConnection.Query<Customer>("[dbo].[SaveCustomer]", parms, null, false, null,
+                    CommandType.StoredProcedure);
+
+                newCustomer = customers.ToList().FirstOrDefault();
+            }
+
+            return newCustomer;
+        }
+    }
+
+}
---
+++ AzureAppServiceDemo/AzureAppServiceDemoApi/Models/Customer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace AzureAppServiceDemoApi.Models
+{
+    public class Customer
+    {
+        [Required]
+        public Int32 Id { get; set; }
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public string Phone { get; set; }
+
+        public Int32 Status { get; set; }
+
+    }
+}
